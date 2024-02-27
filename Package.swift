d03f0ac30cb9c32bad66e11fd6f5083dd4f4b4a4
Pierluigi Cifani <pcifani@theleftbit.com>
2024-02-27 16:55:15 +0100

    Bump this

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MediQuoSDK",
-            url: "https://filebin.net/uelp9pd8x9m62gvq/MediQuoSDK.xcframework.zip",
-            checksum: "4dd98edad262f2b2adbe026858df30383d7a3146ee8b88341651bad729c4a508"
+            url: "https://github.com/mediquo/mediquo-ios-sdk/releases/download/10.0.4/MediQuoSDK.xcframework.zip",
+            checksum: "845e5d1efc53323e77fb6e7ab2da03bcb4e6195765924bf1c18941d38cda8538"
         )
     ]
 )
