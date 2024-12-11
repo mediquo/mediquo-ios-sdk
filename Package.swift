a67841f2bb53f613b86cb485049ff7f4d1f4a615
Pierluigi Cifani <pcifani@theleftbit.com>
2024-12-11 17:18:01 +0100

    Bump to 10.6.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MediQuoSDK",
-            url: "https://github.com/mediquo/mediquo-ios-sdk/releases/download/10.5.0/MediQuoSDK.xcframework.zip",
-            checksum: "be062a9ad692c3485db2d6e39cdc0759ceacfda8196d8b7f12e248aa8ebb87bd"
+            url: "https://github.com/mediquo/mediquo-ios-sdk/releases/download/10.6.0/MediQuoSDK.xcframework.zip",
+            checksum: "2308c3d1a410e130b570425308949b93d116c1e8038a62562c2d63a27e50cc19"
         )
     ]
 )
